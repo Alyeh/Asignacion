c6a33e713a7107ee39357454b11d21d5e9c23f0e
Alyeluz <Alyeluz@DESKTOP-UFR4GN4>
2023-05-02 11:50:30 -0400

    Se imprime un titulo

---
+++ src/texto/Main.java
@@ -0,0 +1,10 @@
+package texto;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Asignacion de Alyeluz Perez");
+	}
+
+}
